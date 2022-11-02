e858280e368e3fdd755bff3dbc07cc725e465d2d
Luke Multanen <lmultanen.uchicago@gmail.com>
2022-11-02 12:10:36 -0500

    made logos the hyperlinks

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,17 +5,19 @@ const Contact = () => {
         <div id="contact-page">
             {/* Feel free to input your contact information and a message if you would like to contact me, and I will get back to you as soon as possible! */}
             <h2>Feel free to reach out or connect with me through LinkedIn!</h2>
-            <img src='/bitmojis/envelope-greeting.png'/>
+            <img id="contact-img" src='/bitmojis/envelope-greeting.png'/>
             <br/>
             <div>
                 <div className='contact-container'>
-                    <div className='contact-link'>
-                        <img className="small-logo" src="/LinkedIn_icon.svg.png" alt="Logo" /> 
-                        <a target={'_blank'} href='https://www.linkedin.com/in/lukemultanen/'>LinkedIn</a>
+                    <div className='contact-link'>          
+                        <a target={'_blank'} href='https://www.linkedin.com/in/lukemultanen/'>
+                            <img className="small-logo" src="/LinkedIn_icon.svg.png" alt="Logo" /> 
+                        </a>
                     </div>
-                    <div className='contact-link'>
-                        <img className="small-logo" src="/GitHub-Mark-120px-plus.png" alt="Logo" /> 
-                        <a target={'_blank'} href='https://github.com/lmultanen'>GitHub</a>
+                    <div className='contact-link'> 
+                        <a target={'_blank'} href='https://github.com/lmultanen'>
+                            <img className="small-logo" src="/GitHub-Mark-120px-plus.png" alt="Logo" />
+                        </a>
                     </div>
                 </div>
             </div>
